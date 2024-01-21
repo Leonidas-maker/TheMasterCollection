859d53990c15bef6205c8e0fd65f42bc080ce91a
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2024-01-21 22:45:01 +0100

    added basic imprint informations

--- client/src/pages/imprint/imprint.tsx
+++ client/src/pages/imprint/imprint.tsx
@@ -4,6 +4,11 @@ function Imprint() {
     return (
         <div className="text-2xl test-black">
             <h1>Welcome to Imprint</h1>
+            <br/>
+            <p className="text-lg">Leon Sylvester</p>
+            <p className="text-lg">Mainzer Landstraße 231</p>
+            <p className="text-lg">60326 Frankfurt am Main</p>
+            <p className="text-lg">support@theshopmaster.de</p>
         </div>
     );
 }

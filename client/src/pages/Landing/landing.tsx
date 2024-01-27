ce224e4e9021ff00eac0fe7ac444f9721f94e9a2
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2024-01-27 21:06:38 +0100

    removed one warning and added the new icons

--- client/src/pages/Landing/landing.tsx
+++ client/src/pages/Landing/landing.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Navbar from "../../components/navbar/navbar";
-import { useTranslation } from "react-i18next";
 import { Footer } from "../../components/footer";
 import { CarouselProducts } from "../../components/carousel";
 
 function Landing() {
-    //! Add Popup informations
-    const { t } = useTranslation();
-
     return (
         <div className="h-screen bg-white dark:bg-gray-900">
             <Navbar />
